ba91ff9a2fe5504dc3ee376182bd07d51a51c42a
Evgeny Kuznetsov <45016259+betatester25@users.noreply.github.com>
2026-01-08 13:01:11 +0300

    Add files via upload

---
+++ trigger_sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'trigger-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Установка Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Кеш открыт');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Удаление старого кеша:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Обработка запросов
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Возвращаем кешированный ответ или загружаем из сети
+        return response || fetch(event.request);
+      })
+  );
+});
